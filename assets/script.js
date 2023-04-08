79fea90a91c6c7cd1df5d8d225ef107039afd8e0
lexrayne <lexrayne@outlook.com>
2023-04-07 21:23:42 -0300

    Added JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,38 @@
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
 var shapes = ["triangle", "square", "pentagon", "circle"];
 
-for (x = 0; x < shapes.length; x++) {
-  console.log(shapes[x])
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic(randomTopic);
+
+function helloWorld() {
+  console.log("Hello world, I am a function!")
+}
+
+function listTopics() {
+  for (x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+  }
+}
+
+function selectTopic(topic) {
+  if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  }
+  else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  }
+  else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  }
+  else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  }
+  else {
+   console.log('Please try again!');
+  }
 }
 
 /*
